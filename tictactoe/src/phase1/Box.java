4c646950f024677c4a3615b64a5e5eef6ce5e429
Sujin Lee <sujinbonnaud@gmail.com>
2023-09-26 11:19:11 -0500

    create basic 5 classes

---
+++ tictactoe/src/phase1/Box.java
@@ -0,0 +1,10 @@
+package phase1;
+
+public class Box {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ tictactoe/src/phase1/DriverMain.java
@@ -0,0 +1,10 @@
+package phase1;
+
+public class DriverMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
