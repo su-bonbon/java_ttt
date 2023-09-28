3890be5fd3f11db5c9268b6294ae12396d225bf5
Sujin Lee <sujinbonnaud@gmail.com>
2023-09-27 20:49:42 -0500

    create Box class

--- tictactoe/src/phase1/Box.java
+++ tictactoe/src/phase1/Box.java
@@ -1,10 +1,36 @@
 package phase1;
 
 public class Box {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int row;
+	private int col;
+	private String placeHolder = Box.DASH;
+	public final static String DASH = "-";
+	
+	Box(int row, int col){
+		this.row = row;
+		this.col = col;
 	}
 
+	String getPlaceHolder() {
+		return placeHolder;
+	}
+
+	boolean setPlaceHolder(String placeHolder) {
+		if(isAvailable()) {
+			this.placeHolder = placeHolder;
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isAvailable() {
+		return this.placeHolder.equals(Box.DASH);
+	}
+
+	void print() {
+		System.out.print(this.placeHolder + " ");
+	}
+	
+	
+	
 }
